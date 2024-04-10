a5a4132bc1b0773402ecb5fb369db724ce932c73
EmanuelTabian <emanuel.forwork@gmail.com>
2024-04-10 13:05:03 +0300

    Define the logo and stylize it

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Income />
+      <ExpenseForm />
+      <Output />
+    </div>
+  );
+}
 
+function Logo() {
+  return (
+    <h1 className="logo">
+      <span className="logo-color">RIDESHARE</span>&nbsp;
+      <span>COPILOT</span>
+    </h1>
+  );
+}
+function Income() {}
+function ExpenseForm() {}
+function Output() {}
